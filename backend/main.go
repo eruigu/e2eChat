513796c87b288b801d1a4f7eb5f8bb949c89b1b3
etr <erick@ruigu.dev>
2026-03-17 00:02:24 -0500

    Cleaning up and optimizing dev env

--- backend/main.go
+++ backend/main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"log"
-	"os"
+	// "os"
 )
 
 func main() {
-	log.SetFlags(0)
+	// log.SetFlags(0)
 
-	if len(os.Args) < 2 {
-		log.Fatal("usage: go run . [server|client]")
-	}
-
-	switch os.Args[1] {
-	case "server":
+	// if len(os.Args) < 2 {
+	// 	log.Fatal("usage: go run . [server|client]")
+	// }
+	//
+	// switch os.Args[1] {
+	// case "server":
 		if err := runServer(); err != nil {
 			log.Fatal(err)
-		}
-	case "client":
-		RunClient()
-	default:
-		log.Fatalf("unknown command: %s", os.Args[1])
-	}
+		};
+	// case "client":
+	// 	RunClient()
+	// default:
+	// 	log.Fatalf("unknown command: %s", os.Args[1])
+	// }
+
 }
